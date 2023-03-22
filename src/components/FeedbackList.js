d9e98a2eebae5272de2252c5e7958cf4ab905268
VVKo <v.v.kovdrysh@gmail.com>
2023-03-22 12:23:17 +0200

    add Spinner
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -2,26 +2,27 @@ import React, {useContext} from "react";
 import { motion } from "framer-motion"
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "../shared/Spinner";
 
 const FeedbackList = () => {
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>Повідомлень немає</p>;
   }
-  return (
-    <div className="feedback-list">
-      {feedback.map((item) => (
-          <motion.div key={item.id}
-                      initial={{opacity: 0}}
-                      animate={{opacity: 1}}
-                      exit={{opacity: 0}} >
-        <FeedbackItem
-            item={item}/>
-          </motion.div>
-      ))}
-    </div>
-  );
+
+  return isLoading ? <Spinner /> : (<div className="feedback-list">
+    {feedback.map((item) => (
+        <motion.div key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}} >
+          <FeedbackItem
+              item={item}/>
+        </motion.div>
+    ))}
+  </div>)
+
 };
 
 export default FeedbackList;
